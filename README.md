11c98cf67886b93883ad80c1f2bd7d588716d878
Chase Goldfeld <goldfeld.chase@gmail.com>
2020-05-18 16:21:20 -0400

    implemented golden cross strategy

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# au_cross
-Golden Cross Strategy
+# AU_Cross
+Golden Cross Strategy vs Buy & Hold
+
+To run the two different strategies for SPY:
+in terminal = python3 run.py {}
+
+ if {}:
+    input one of two strategies
+        1. au_cross
+        2. BuyHold
+
---
+++ run.py
@@ -0,0 +1,32 @@
+import os, sys, argparse
+import pandas as pd 
+import backtrader as bt 
+import matplotlib as plt 
+from strategies.au_cross import AU_Cross
+from strategies.BuyHold import BuyHold
+
+strategies = {
+    "au_cross": AU_Cross, 
+    "buy_hold": BuyHold
+
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("strategy", help="which strategy to run", type=str)
+args = parser.parse_args()
+
+if not args.strategy in strategies: 
+    print("invalid strategy, must be one of {}".format(strategies.keys()))
+    sys.exit()
+
+cerebro = bt.Cerebro()
+cerebro.broker.setcash(100000)
+
+spy_prices = pd.read_csv('data/SPY.csv', index_col="Date", parse_dates=True)
+
+feed = bt.feeds.PandasData(dataname=spy_prices)
+cerebro.adddata(feed)
+
+cerebro.addstrategy(strategies[args.strategy])
+cerebro.run()
+cerebro.plot()
